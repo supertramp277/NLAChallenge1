3163729a58fb65951d7ddc45292bc5dd0c2673c7
supertramp277 <18621056089@163.com>
2024-10-06 11:23:16 +0000

    Add Github Link To Cpp File Annotation

--- Challenge1.cpp
+++ Challenge1.cpp
@@ -7,7 +7,9 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-
+/*****************************************************************************************************************/
+// More details and explanation are at this link: https://github.com/supertramp277/NLAChallenge1/tree/main
+/*****************************************************************************************************************/
 using namespace Eigen;
 typedef Eigen::Triplet<double> T;
 
